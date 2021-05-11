97d3fc04ee6cdcd7865f540ea9cd01763671261d
OderoOluoch <oderoluoch@gmail.com>
2021-05-11 14:20:53 +0300

    eleganly simple

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -4,33 +4,56 @@ import { Todo } from './../../models/Todo';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
+  todos: Todo[];
 
-  todos:Todo[];
+  inputTodo:string = "";
 
-  constructor() { }
-
+  constructor() {}
   ngOnInit(): void {
     this.todos = [
       {
-        content:'First todo',
-        completed: false
-      },{
-        content:'Second todo',
-        completed: false
-      },{
-        content:'Third todo',
-        completed: false
-      },{
-        content:'Fourth todo',
-        completed: false
-      },{
-        content:'Fifth todo',
-        completed: false
-      }
-    ]
+        content: 'First todo',
+        completed: false,
+      },
+      {
+        content: 'Second todo',
+        completed: true,
+      },
+      {
+        content: 'Third todo',
+        completed: false,
+      },
+      {
+        content: 'Fourth todo',
+        completed: true,
+      },
+      {
+        content: 'Fifth todo',
+        completed: false,
+      },
+    ];
   }
 
+  toggleDone(id: number) {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    });
+  }
+
+  deleteTodo(id:number){
+    this.todos = this.todos.filter((v,i) => i !==id);
+  }
+  addTodo(){
+    this.todos.push({
+      content:this.inputTodo,
+      completed: false
+    });
+
+    this.inputTodo = "";
+  }
 }
